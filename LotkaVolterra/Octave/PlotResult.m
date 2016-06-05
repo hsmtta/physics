6287906bf519008aa232edf70eeac0cb46f7ef63
Takahiro Hashimoto <hashimoto.takahiro17@gmail.com>
2016-06-05 21:12:13 +0900

    simulate with conservation quantity

--- LotkaVolterra/Octave/PlotResult.m
+++ LotkaVolterra/Octave/PlotResult.m
@@ -2,7 +2,7 @@
 % (c) 2016 Takahiro Hashimoto
 %
 
-data = dlmread('../result.dat', '', 2, 0);
+data = dlmread('../build/result.dat', '', 2, 0);
 
 figure(1);
 plot(data(:,1), data(:,2), data(:,1), data(:,3));
@@ -15,4 +15,3 @@ plot(data(:,1), data(:, 4));
 grid on;
 xlabel('Time');
 ylabel('V');
-axis([0 100 -3 0]);
--- LotkaVolterra/src/main.cpp
+++ LotkaVolterra/src/main.cpp
@@ -7,15 +7,18 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <math.h>
 #include <gsl/gsl_sf_lambert.h>
+
+using namespace std;
 
 const int TimeStepMax = 10000;
 
 const double dt = 0.01;
 
-const double popInit1 = 0.9;
-const double popInit2 = 0.9;
+const double popInit1 = 1.5;
+const double popInit2 = 1.0;
 
 const double a = 2./3;
 const double b = 4./3;
@@ -25,7 +28,8 @@ const double d = 1.;
 const string ResultFilename = "result.dat";
 
 void PrintUsageAndExit(const string& arg0);
-double W0Extended(const double x);
+double W1Extended(const double x, bool& isInsideRange);
+double W0Extended(const double x, bool& isInsideRange);
 
 int main(int argc, char const *argv[])
 {
@@ -58,8 +62,8 @@ int main(int argc, char const *argv[])
 	pop1[0] = popInit1;
 	pop2[0] = popInit2;
 
-	double v0 = -d*popInit1 + c*log(popInit1)
-				-b*popInit2 + a*log(popInit2);
+	double v0 = -a*log(popInit2) + b*popInit2
+				-c*log(popInit1) + d*popInit1;
 
 	// update population
 	for ( int tIdx = 0; tIdx < TimeStepMax-1; tIdx++)
@@ -68,40 +72,59 @@ int main(int argc, char const *argv[])
 		pop1[tIdx+1] =
 			(a*pop1[tIdx] - b*pop1[tIdx]*pop2[tIdx])*dt + pop1[tIdx];
 
+		pop2[tIdx+1] =
+			(-c*pop2[tIdx] + d*pop1[tIdx]*pop2[tIdx])*dt + pop2[tIdx];
+
 		if ( isUseConservedQuantity )
 		{
 			const double p1 = pop1[tIdx+1];
-			double p2 = W0Extended(-b/a*exp((v0-d*log(p1)+c*p1)/a));
-			p2 = -a/b*p2;
-			pop2[tIdx+1] = p2;
-		} 
-		else
-		{
-			pop2[tIdx+1] =
-				(-c*pop2[tIdx] + d*pop1[tIdx]*pop2[tIdx])*dt + pop2[tIdx];	
+			const double p2 = pop2[tIdx+1];
+			double p2_;
+			bool isInsideRange;
+
+			if ( b/a*p2 > 1 )
+			{
+				//cout << "m1 branch" << "\n";
+				p2_ = -a/b*W1Extended(
+					-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
+					isInsideRange );
+			}
+			else
+			{
+				//cout << "0 branch" << "\n";
+				p2_ = -a/b*W0Extended(
+					-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
+					isInsideRange );
+			}
+
+			if ( isInsideRange ) pop2[tIdx+1] = p2_;
 		}
 	}
 
 	double v[TimeStepMax];
 	for ( int tIdx = 0; tIdx < TimeStepMax; tIdx++)
 	{
-		v[tIdx] = -d*pop1[tIdx] + c*log(pop1[tIdx])
-				  -b*pop2[tIdx] + a*log(pop2[tIdx]);
+		v[tIdx] = -a*log(pop2[tIdx]) + b*pop2[tIdx]
+				  -c*log(pop1[tIdx]) + d*pop1[tIdx];
 	}
 
 	// save result
-
 	cout << "save result as " << ResultFilename << "\n";
 
-	cout << "# population evolution simulated by Lotka-Volterra equations" << "\n";
-	cout << "# t pop1 pop2 v" << "\n";
+	ofstream ofs;
+	ofs.open(ResultFilename.c_str());
+
+	ofs << "# population evolution simulated by Lotka-Volterra equations" << "\n";
+	ofs << "# t pop1 pop2 v" << "\n";
 	for ( int tIdx = 0; tIdx < TimeStepMax; tIdx++)
 	{
-		std::cout << t[tIdx] << " "
-				  << pop1[tIdx] << " "
-				  << pop2[tIdx] << " "
-				  << v[tIdx] << "\n";
+		ofs << t[tIdx] << " "
+			<< pop1[tIdx] << " "
+			<< pop2[tIdx] << " "
+			<< v[tIdx] << "\n";
 	}
+
+	ofs.close();
 
 	return 0;
 }
@@ -113,7 +136,28 @@ void PrintUsageAndExit(const string& arg0)
 	cout << "Options: -c                use conserved quantity" << "\n";
 }
 
-double W0Extended(const double x)
+double W1Extended(const double x, bool& isInsideRange)
+{
+
+	gsl_sf_result result;
+	int status = gsl_sf_lambert_Wm1_e(x, &result);
+
+	double y;
+	if ( status != 0 )
+	{
+		y = -1.;
+		isInsideRange = false;
+	} 
+	else
+	{
+		y = result.val;
+		isInsideRange = true;
+	}
+
+	return y;
+}
+
+double W0Extended(const double x, bool& isInsideRange)
 {
 
 	gsl_sf_result result;
@@ -123,10 +167,12 @@ double W0Extended(const double x)
 	if ( status != 0 )
 	{
 		y = -1.;
+		isInsideRange = false;
 	} 
 	else
 	{
 		y = result.val;
+		isInsideRange = true;
 	}
 
 	return y;
