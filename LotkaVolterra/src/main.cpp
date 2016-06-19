cff6e5377a0085ee7962c6df18153ac9ceab2505
Takahiro Hashimoto <hashimoto.takahiro17@gmail.com>
2016-06-19 20:47:50 +0900

    add runtime option

--- LotkaVolterra/src/main.cpp
+++ LotkaVolterra/src/main.cpp
@@ -14,26 +14,24 @@
 using namespace std;
 
 const int TimeStepMax = 10000;
-
 const double dt = 0.01;
-
 const string ResultFilename = "result.dat";
 
 void PrintUsageAndExit(const string& arg0);
-double W1Extended(const double x, bool& isInsideRange);
+double Wm1Extended(const double x, bool& isInsideRange);
 double W0Extended(const double x, bool& isInsideRange);
 
 int main(int argc, char const *argv[])
 {
-	const double popInit1 = 1.5;
-	const double popInit2 = 1.0;
+	double popInit1 = 1.5;
+	double popInit2 = 1.0;
 
 	double a = 2./3;
 	double b = 4./3;
 	double c = 1.;
 	double d = 1.;
 
-	bool isUseConservedQuantity = false;
+	bool isUseInvariant = false;
 
 	// process command line arguments
 	for (int argIdx = 1; argIdx < argc; argIdx++)
@@ -42,17 +40,36 @@ int main(int argc, char const *argv[])
 		{
 			PrintUsageAndExit(argv[0]);
 		}
-		else if ( string(argv[argIdx]) == "-c" )
+		else if ( string(argv[argIdx]) == "-i" )
 		{
-			cout << "simulate with conserved quantity" << "\n";
-			isUseConservedQuantity = true;
+			cout << "simulate with invariant" << "\n";
+			isUseInvariant = true;
 		}
 		else if ( string(argv[argIdx]) == "-p" )
 		{
+			cout << "set parameters" << "\n";
 			if ( argIdx + 4 < argc )
 			{
-				
-			} else
+				a = atof(argv[argIdx+1]);
+				b = atof(argv[argIdx+2]);
+				c = atof(argv[argIdx+3]);
+				d = atof(argv[argIdx+4]);
+				argIdx = argIdx + 4;
+			} 
+			else
+			{
+				PrintUsageAndExit(argv[0]);
+			}
+		}
+		else if ( string(argv[argIdx]) == "-n" ) 
+		{
+			if ( argIdx + 2 < argc )
+			{
+				popInit1 = atof(argv[argIdx+1]);
+				popInit2 = atof(argv[argIdx+2]);
+				argIdx = argIdx + 2;
+			}
+			else
 			{
 				PrintUsageAndExit(argv[0]);
 			}
@@ -62,6 +79,15 @@ int main(int argc, char const *argv[])
 			PrintUsageAndExit(argv[0]);
 		}
 	}
+
+	cout << "simulation parameters: a = " << a 
+		 << ", b = " << b
+		 << ", c = " << c
+		 << ", d = " << d << "\n";
+	cout << "initial populations: pop1 = " << popInit1
+		 << ", pop2 = " << popInit2 << "\n";
+
+	// init glut
 
 	double t[TimeStepMax];
 	double pop1[TimeStepMax], pop2[TimeStepMax];
@@ -84,7 +110,7 @@ int main(int argc, char const *argv[])
 		pop2[tIdx+1] =
 			(-c*pop2[tIdx] + d*pop1[tIdx]*pop2[tIdx])*dt + pop2[tIdx];
 
-		if ( isUseConservedQuantity )
+		if ( isUseInvariant )
 		{
 			const double p1 = pop1[tIdx+1];
 			const double p2 = pop2[tIdx+1];
@@ -94,7 +120,7 @@ int main(int argc, char const *argv[])
 			if ( b/a*p2 > 1 )
 			{
 				//cout << "-1 branch" << "\n";
-				p2_ = -a/b*W1Extended(
+				p2_ = -a/b*Wm1Extended(
 					-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
 					isInsideRange );
 			}
@@ -140,12 +166,17 @@ int main(int argc, char const *argv[])
 
 void PrintUsageAndExit(const string& arg0)
 {
-	cout << "Usage: " << arg0 << "[options]" << "\n";
-	cout << "Options: -h | --help       print this usage" << "\n";
-	cout << "Options: -c                use conserved quantity" << "\n";
+	cout << "Usage: " << arg0 << " <options>" << "\n";
+	cout << "Options:" << "\n";
+	cout << "-h | --help          print this usage" << "\n";
+	cout << "-i                   use invariant" << "\n";
+	cout << "-p <a> <b> <c> <d>   set parameters a, b, c and d" << "\n";
+	cout << "-n <p1> <p2>         set initial population p1, p2" << "\n";
+
+	exit(0);
 }
 
-double W1Extended(const double x, bool& isInsideRange)
+double Wm1Extended(const double x, bool& isInsideRange)
 {
 
 	gsl_sf_result result;
