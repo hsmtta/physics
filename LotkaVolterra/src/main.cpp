409d3fb02dedd44812eb8691b2ce321d80b0b5d7
Takahiro Hashimoto <hashimoto.takahiro17@gmail.com>
2016-06-25 13:47:38 +0900

    close nuko

--- LotkaVolterra/src/main.cpp
+++ LotkaVolterra/src/main.cpp
@@ -10,29 +10,43 @@
 #include <fstream>
 #include <math.h>
 #include <gsl/gsl_sf_lambert.h>
+#include <GL/glut.h>
 
 using namespace std;
 
-const int TimeStepMax = 10000;
+const int TimeBuff = 10000;
 const double dt = 0.01;
 const string ResultFilename = "result.dat";
+
+double a, b, c, d;
+double v0;
+
+bool isUseInvariant = false;
+
+double time[TimeBuff];
+double pop1[TimeBuff], pop2[TimeBuff];
 
 void PrintUsageAndExit(const string& arg0);
 double Wm1Extended(const double x, bool& isInsideRange);
 double W0Extended(const double x, bool& isInsideRange);
+double getInvariant(const double p1, const double p2);
+void update(const double t, const double p1, const double p2,  
+	        double &tn, double &pn1, double &pn2);
 
-int main(int argc, char const *argv[])
+void display(void);
+void initDisplay(void);
+
+int main(int argc, char *argv[])
 {
 	double popInit1 = 1.5;
 	double popInit2 = 1.0;
 
-	double a = 2./3;
-	double b = 4./3;
-	double c = 1.;
-	double d = 1.;
-
-	bool isUseInvariant = false;
 	bool isUseGlut = false;
+
+	a = 2./3;
+	b = 4./3;
+	c = 1.;
+	d = 1.;
 
 	// process command line arguments
 	for (int argIdx = 1; argIdx < argc; argIdx++)
@@ -92,79 +106,62 @@ int main(int argc, char const *argv[])
 	cout << "initial populations: pop1 = " << popInit1
 		 << ", pop2 = " << popInit2 << "\n";
 
-	// init glut
 
-	double t[TimeStepMax];
-	double pop1[TimeStepMax], pop2[TimeStepMax];
+	v0 = getInvariant(popInit1, popInit2);
 
-	// initialize t, pop1, pop2
-	t[0] = 0;
+	time[0] = 0;
 	pop1[0] = popInit1;
 	pop2[0] = popInit2;
 
-	double v0 = -a*log(popInit2) + b*popInit2
-				-c*log(popInit1) + d*popInit1;
-
-	// update population
-	for ( int tIdx = 0; tIdx < TimeStepMax-1; tIdx++)
+	if ( isUseGlut )
 	{
-		t[tIdx + 1] = dt + t[tIdx];
-		pop1[tIdx+1] =
-			(a*pop1[tIdx] - b*pop1[tIdx]*pop2[tIdx])*dt + pop1[tIdx];
+		// init glut
+		glutInit(&argc, argv);
+		glutCreateWindow(argv[0]);
+		glutDisplayFunc(display);
 
-		pop2[tIdx+1] =
-			(-c*pop2[tIdx] + d*pop1[tIdx]*pop2[tIdx])*dt + pop2[tIdx];
+		initDisplay();
 
-		if ( isUseInvariant )
+		glutMainLoop();
+	}
+	else
+	{
+		// simulate and save result
+		double v[TimeBuff];
+		for ( int tIdx = 0; tIdx < TimeBuff-1; tIdx++)
 		{
-			const double p1 = pop1[tIdx+1];
-			const double p2 = pop2[tIdx+1];
-			double p2_;
-			bool isInsideRange;
+			// update population
+			const double t = time[tIdx];
+			const double p1 = pop1[tIdx];
+			const double p2 = pop2[tIdx];
+			double tn, pn1, pn2;
+			update(t, p1, p2, tn, pn1, pn2);
+			time[tIdx+1] = tn;
+			pop1[tIdx+1] = pn1;
+			pop2[tIdx+1] = pn2;
 
-			if ( b/a*p2 > 1 )
-			{
-				//cout << "-1 branch" << "\n";
-				p2_ = -a/b*Wm1Extended(
-					-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
-					isInsideRange );
-			}
-			else
-			{
-				//cout << "0 branch" << "\n";
-				p2_ = -a/b*W0Extended(
-					-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
-					isInsideRange );
-			}
+			v[tIdx+1] = getInvariant(p1, p2);
 
-			if ( isInsideRange ) pop2[tIdx+1] = p2_;
 		}
+
+		// save result
+		cout << "save result as " << ResultFilename << "\n";
+
+		ofstream ofs;
+		ofs.open(ResultFilename.c_str());
+
+		ofs << "# population evolution simulated by Lotka-Volterra equations" << "\n";
+		ofs << "# t pop1 pop2 v" << "\n";
+		for ( int tIdx = 0; tIdx < TimeBuff; tIdx++)
+		{
+			ofs << time[tIdx] << " "
+				<< pop1[tIdx] << " "
+				<< pop2[tIdx] << " "
+				<< v[tIdx] << "\n";
+		}
+
+		ofs.close();
 	}
-
-	double v[TimeStepMax];
-	for ( int tIdx = 0; tIdx < TimeStepMax; tIdx++)
-	{
-		v[tIdx] = -a*log(pop2[tIdx]) + b*pop2[tIdx]
-				  -c*log(pop1[tIdx]) + d*pop1[tIdx];
-	}
-
-	// save result
-	cout << "save result as " << ResultFilename << "\n";
-
-	ofstream ofs;
-	ofs.open(ResultFilename.c_str());
-
-	ofs << "# population evolution simulated by Lotka-Volterra equations" << "\n";
-	ofs << "# t pop1 pop2 v" << "\n";
-	for ( int tIdx = 0; tIdx < TimeStepMax; tIdx++)
-	{
-		ofs << t[tIdx] << " "
-			<< pop1[tIdx] << " "
-			<< pop2[tIdx] << " "
-			<< v[tIdx] << "\n";
-	}
-
-	ofs.close();
 
 	return 0;
 }
@@ -223,8 +220,70 @@ double W0Extended(const double x, bool& isInsideRange)
 	return y;
 }
 
+double getInvariant(const double p1, const double p2)
+{
+	return -a*log(p2) + b*p2 -c*log(p1) + d*p1;
+}
 
+void update(const double t, const double p1, const double p2, 
+	        double &tn, double &pn1, double &pn2)
+{
+	tn = dt + t;
+	pn1 = (a*p1 - b*p1*p2)*dt + p1;
+	pn2 = (-c*pn2 + d*p1*p2)*dt + p2;
 
+	if ( isUseInvariant )
+	{
+		double pn2_;
+		bool isInsideRange;
 
+		if ( b/a*p2 > 1 )
+		{
+			pn2_ = -a/b*Wm1Extended(
+				-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
+				isInsideRange );
+		}
+		else
+		{
+			pn2_ = -a/b*W0Extended(
+				-b/a*(exp(1./a*(-c*log(p1)+d*p1-v0))), 
+				isInsideRange );
+		}
 
+		if ( isInsideRange ) pn2 = pn2_;
+	}
+}
 
+void display(void)
+{
+	static int count = 0;
+
+	const double t = time[count];
+	const double p1 = pop1[count];
+	const double p2 = pop2[count];
+
+	double tn, pn1, pn2;
+	update(p1, p2, t, pn1, pn2, tn);
+
+	if ( ++count == TimeBuff -1) count = 0;
+
+	time[count] = tn;
+	pop1[count] = pn1;
+	pop2[count] = pn2;
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glLineWidth(2.5);
+	glColor3f(1.0, 0.0, 0.0);
+	glBegin(GL_LINES);
+	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(15, 0, 0);
+	glEnd();
+
+	glFlush();
+}
+
+void initDisplay(void)
+{
+	glClearColor(0.0, 0.0, 1.0, 1.0);
+}
